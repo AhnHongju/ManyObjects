8cb7226499ae62fb7153c9e242de4cfbfd6e30c3
AhnHongju <93176798+AhnHongju@users.noreply.github.com>
2021-11-02 16:20:18 +0900

    final

--- sketch.js
+++ sketch.js
@@ -1,16 +1,36 @@
-let m;
+let m, m2;
+let movers = [];
+let canvas;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  canvas = createGraphics(width, height);
   m = new Mover(50, 50, 10);
+  m2 = new Mover(300, 300, 50);
+
+  for (let i=0; i<5; i++) {
+    let mover = new Mover(random(width), random(height), random(5, 50));
+    movers.push(mover);
+  }
 }
 
 function draw() {
   background(220);
-  m.update();
-  m.show();
+  image(canvas, 0, 0);
+
+  for (let mover of movers) {
+    mover.update();
+    mover.show();
+    mover.drawOn(canvas);
+  }
+
 
   if (mouseIsPressed) {
-    m.pos.set(mouseX,mouseY);
+    //make attract to mouse position
+    let mousePos = createVector(mouseX, mouseY);
+
+    for (let mover of movers) {
+      mover.attractTo(mousePos);
+    }
   }
 }
