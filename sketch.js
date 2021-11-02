5adc7e61e65045de7f0c566912e7d0617db4883f
AhnHongju <93176798+AhnHongju@users.noreply.github.com>
2021-11-02 14:22:26 +0900

    mover.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
-function setup()
-{
+let m;
+
+function setup() {
   createCanvas(400, 400);
-  background(220);
-  text("Hello world",200,200);
+  m = new Mover(50, 50, 10);
 }
 
-function draw()
-{
-
+function draw() {
+  background(220);
+  m.update();
+  m.show();
+  
+  if (mouseIsPressed) {
+    //make attract to mouse position
+  }
 }
